3e90d7358a17d16a307320b68ce54ee3c6fbdc41
Seokhyun Kim <wjapp72@gmail.com>
2020-11-03 20:59:12 -0800

    Initial Commit

---
+++ Extension_practice/Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Extension.swift
+//  Extension_practice
+//
+//  Created by Seokhyun Kim on 2020-11-03.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func description() {
+        print("This is an extension for UIViewController")
+    }
+}
---
+++ Extension_practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Extension_practice
+//
+//  Created by Seokhyun Kim on 2020-11-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        description()
+    }
+
+
+}
+
